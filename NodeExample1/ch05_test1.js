c0df7b501a28228fa90900fce32660159c08aa12
cyBisu <k39335@naver.com>
2019-09-17 21:19:42 +0900

    :pencil: modify listen function

--- NodeExample1/ch05_test1.js
+++ NodeExample1/ch05_test1.js
@@ -4,7 +4,14 @@ var http = require("http");
 var server = http.createServer();
 
 // 웹 서버를 시작하여 3000번 포트에서 대기합니다.
+// var port = 3000;
+// server.listen(port, function() {
+//   console.log("웹 서버가 시작되었습니다. : %d", port);
+// });
+
+// 웹 서버를 시작하여 172.30.1.11 IP와 3000번 포트에서 대기하도록 설정합니다.
+var host = "172.30.1.11";
 var port = 3000;
-server.listen(port, function() {
-  console.log("웹 서버가 시작되었습니다. : %d", port);
+server.listen(port, host, "50000", function() {
+  console.log("웹 서버가 시작되었습니다. : %s, %d", host, port);
 });
